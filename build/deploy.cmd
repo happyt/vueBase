051d3f502803905d7146a92ae9ce7680ce95cd08
ian morton <ian.happytuesdays@gmail.com>
2018-01-13 14:56:35 +0000

    Spellings

--- build/deploy.cmd
+++ build/deploy.cmd
@@ -1,14 +1,14 @@
 call npm install
 call npm run build
 
-call cd %DEPLOYMENT_TARGWTS%
-for /F "delims=" %%i i ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+call cd %DEPLOYMENT_TARGET%
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
 
-xcopy /d %DEPLOYMENT_SOURCE%\dist\* %DEPLOYMENT_TARGWTS%\dist /s /i
-xcopy /d %DEPLOYMENT_SOURCE%\index.html %DEPLOYMENT_TARGWTS%\index.html*
-xcopy /d %DEPLOYMENT_SOURCE%\server.js %DEPLOYMENT_TARGWTS%\server.js*
-xcopy /d %DEPLOYMENT_SOURCE%\package.json%DEPLOYMENT_TARGWTS%\package.json*
-xcopy /d %DEPLOYMENT_SOURCE%\web.config* %DEPLOYMENT_TARGWTS%\web.config*
+xcopy /d %DEPLOYMENT_SOURCE%\dist\* %DEPLOYMENT_TARGET%\dist /s /i
+xcopy /d %DEPLOYMENT_SOURCE%\index.html %DEPLOYMENT_TARGET%\index.html*
+xcopy /d %DEPLOYMENT_SOURCE%\server.js %DEPLOYMENT_TARGET%\server.js*
+xcopy /d %DEPLOYMENT_SOURCE%\package.json %DEPLOYMENT_TARGET%\package.json*
+xcopy /d %DEPLOYMENT_SOURCE%\web.config %DEPLOYMENT_TARGET%\web.config*
 
 call npm install --only=production
 echo Deployed.
